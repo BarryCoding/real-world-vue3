73b0c3c1ce57de61614bb182dc8325142f1c1d75
Springer <zxllks@yahoo.com>
2023-07-23 16:39:30 +0800

    L5 Axios

---
+++ src/services/EventService.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const APIClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+// Be careful about
+// export {} and export default {}
+export default {
+  getEvents() {
+    return APIClient.get('/events')
+  }
+}
